c9db34104d678ae6071241c25db7f2600059d7d5
HikariJadeEmpire <rastuniing@hotmail.com>
2025-06-15 15:29:25 +0700

    Update UI

--- lib/MyPage.dart
+++ lib/MyPage.dart
@@ -107,23 +107,23 @@ class _MyWeatherApp extends State<MyWeatherApp> {
             ],
           
             flexibleSpace: FlexibleSpaceBar(
-                title: Container(
-                  // color: secondColr,
-                  padding: EdgeInsets.all(5.0),
-                  width: 140,
-                  
-                  child: Text(
-                    'WEATHER\nOBSERVATION',
-                    textAlign: TextAlign.justify,
-                    style: GoogleFonts.inconsolata(
-                      fontSize: 19,
-                      color: primaryColr,
-                      fontWeight: FontWeight.bold,
-                      ),
+              title: Container(
+                // color: secondColr,
+                padding: EdgeInsets.all(5.0),
+                width: 140,
+                
+                child: Text(
+                  'WEATHER\nOBSERVATION',
+                  textAlign: TextAlign.justify,
+                  style: GoogleFonts.inconsolata(
+                    fontSize: 19,
+                    color: primaryColr,
+                    fontWeight: FontWeight.bold,
                     ),
                   ),
+                ),
 
-              // background: FlutterLogo(),
+            // background: FlutterLogo(),
 
             ),
           ),
@@ -135,18 +135,34 @@ class _MyWeatherApp extends State<MyWeatherApp> {
           SliverToBoxAdapter(
               child: Column(
                 children: [
+
+                  Container(
+                    // color: secondHlColr,
+                    margin: EdgeInsets.all(6.0),
+                    height: 40,
+                    
+                    child: Text(
+                      'LOCATION : Bangkok, Thailand',
+                      textAlign: TextAlign.justify,
+                      style: GoogleFonts.inconsolata(
+                        fontSize: 16,
+                        color: primaryColr,
+                        fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
                   
                   Container(
                     color: secondColr,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      spacing: 20,
+                      spacing: 18,
                     
                       children: [
                     
                         Container(
-                          // color: secondColr,
-                          padding: EdgeInsets.all(5.0),
+                          // color: secondHlColr,
+                          padding: EdgeInsets.all(10.0),
                           width: 140,
                           
                           child: Text(
@@ -159,11 +175,44 @@ class _MyWeatherApp extends State<MyWeatherApp> {
                               ),
                             ),
                           ),
-                    
-                        SizedBox(
-                          height: 90,
-                          width: 160,
-                          child: Placeholder(),
+                        
+                        Container(
+                            width: 3,
+                            height: 80,
+                            color: secondHlColr,
+                          ),
+
+                        NumItem(
+                          colr: primaryColr, 
+                          textLabel: 'CELCIUS', 
+                          stringNum: '31',
+                          ),
+                        
+                        Column(
+                          children: [
+                        
+                            Image.asset(
+                              'images/waterfall.gif',
+                              height: 45,
+                            ),
+                        
+                            Container(
+                              height: 40,
+                              width: 80,
+                              padding: EdgeInsets.all(8),
+                              color: secondHlColr,
+                              child: Text(
+                              'Sunny',
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.inconsolata(
+                                fontSize: 16,
+                                color: primaryColr,
+                                fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                            ),
+                        
+                          ],
                         ),
                     
                       ],
@@ -240,11 +289,41 @@ class _MyWeatherApp extends State<MyWeatherApp> {
                             ),
                           ),
                     
-                        SizedBox(
-                          height: 90,
+                        Image.asset(
+                          'images/running.gif',
                           width: 160,
-                          child: Placeholder(),
-                        ),
+                          ),
+                    
+                      ],
+                    
+                    ),
+                  ),
+
+                 Container(
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 18,
+                    
+                      children: [
+                    
+                        NumItem(
+                          colr: primaryColr, 
+                          textLabel: 'Humidity', 
+                          stringNum: '200',
+                          ),
+                    
+                        NumItem(
+                          colr: primaryColr, 
+                          textLabel: 'Wind', 
+                          stringNum: '00',
+                          ),
+
+                        NumItem(
+                          colr: primaryColr, 
+                          textLabel: 'Pressure', 
+                          stringNum: '00',
+                          ),
                     
                       ],
                     
@@ -282,7 +361,53 @@ class _MyWeatherApp extends State<MyWeatherApp> {
             // width: 370,
 
             color: primaryColr,
-            // child: Placeholder(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget> [
+
+                Container(
+                  height: 30,
+                  margin: EdgeInsets.fromLTRB(26, 20, 16, 10),
+                  // color: secondHlColr,
+                  
+                  child: Text(
+                    '// About This Weather App',
+                    textAlign: TextAlign.justify,
+                    style: GoogleFonts.inconsolata(
+                      fontSize: 14,
+                      color: secondColr,
+                      fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    height: 200,
+                    margin: EdgeInsets.only(left: 26, right: 26),
+                    // color: secondHlColr,
+                    child: Text(
+                      '''
+This is a simple yet functional weather application created with the purpose of showcasing programming skills through a real-world project.
+The app includes the following features:
+
+- Temperature Data, Weather Forecast, Humidity Information
+- Wind Speed, Atmospheric Pressure
+- Front-End Design Gimmicks
+
+This app is built to demonstrate a blend of API integration, responsive design, and clean code architecture, making it not just a useful tool, but also a technical portfolio piece.
+                      ''',
+                      textAlign: TextAlign.justify,
+                      style: GoogleFonts.inconsolata(
+                        fontSize: 10,
+                        color: secondColr,
+                        fontWeight: FontWeight.normal,
+                        ),
+                      ),
+
+                  ),
+
+                ],
+              ),
             ),
 
           Row(
@@ -304,7 +429,7 @@ class _MyWeatherApp extends State<MyWeatherApp> {
                   style: GoogleFonts.inconsolata(
                     fontSize: 16,
                     color: secondColr,
-                    fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.normal,
                     ),
                   ),
           
@@ -315,6 +440,62 @@ class _MyWeatherApp extends State<MyWeatherApp> {
       ),
     ),
 
+    );
+  }
+}
+
+//// The stateless widget below can be uses as item which is re-useable
+
+class NumItem extends StatelessWidget {
+
+  final Color colr;
+  final String textLabel;
+  final String stringNum;
+
+  const NumItem({
+    super.key,
+    required this.colr,
+    required this.textLabel,
+    required this.stringNum,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+    
+        SizedBox(
+          height: 45,
+          width: 90,
+    
+          child: Text(
+          stringNum,
+          textAlign: TextAlign.center,
+          style: GoogleFonts.inconsolata(
+            fontSize: 36,
+            color: colr,
+            fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+    
+        Container(
+          height: 40,
+          width: 90,
+          padding: EdgeInsets.all(8),
+          // color: secondHlColr,
+          child: Text(
+          textLabel,
+          textAlign: TextAlign.center,
+          style: GoogleFonts.inconsolata(
+            fontSize: 16,
+            color: colr,
+            fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+    
+      ],
     );
   }
 }
